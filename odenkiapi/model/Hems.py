7f564a1b2dcbbe7623293cbb76a6de79046fd0a3
Takashi SASAKI <takashi316@gmail.com>
2013-06-12 20:24:42 +0900

    isoToNative converts ISO8601 string to native datetime object

--- odenkiapi/model/Hems.py
+++ odenkiapi/model/Hems.py
@@ -60,15 +60,20 @@ class Relays(dict):
         return relays
 
 
-def isoToNativeDateTime(iso_string):
-    pass
-
-
 def awareToNative(dt_aware):
     assert isinstance(dt_aware.tzinfo, datetime.tzinfo)
     utc_time_tuple = dt_aware.utctimetuple()
     dt_native = datetime.datetime(utc_time_tuple.tm_year, utc_time_tuple.tm_mon, utc_time_tuple.tm_mday,
                                   utc_time_tuple.tm_hour, utc_time_tuple.tm_min, utc_time_tuple.tm_sec)
+    return dt_native
+
+
+def isoToNative(iso_string):
+    import dateutil.parser
+
+    dt_aware = dateutil.parser.parse("2013-06-13T19:00:00+09:00")
+    dt_native = awareToNative(dt_aware)
+    assert dt_native.tzinfo is None
     return dt_native
 
 
@@ -85,17 +90,7 @@ class _TestCase(unittest.TestCase):
         self.testbed.init_memcache_stub()
 
     def testDatetime(self):
-        dt_aware = dateutil.parser.parse("2013-06-13T19:00:00+09:00")
-        self.assertEqual("2013-06-13 19:00:00+09:00", str(dt_aware))
-        self.assertIsInstance(dt_aware.tzinfo, datetime.tzinfo)
-
-        # utc_time_tuple = dt_aware.utctimetuple()
-        # dt_native = datetime.datetime(utc_time_tuple.tm_year, utc_time_tuple.tm_mon, utc_time_tuple.tm_mday,
-        #                               utc_time_tuple.tm_hour, utc_time_tuple.tm_min, utc_time_tuple.tm_sec)
-        dt_native = awareToNative(dt_aware)
-        self.assertEqual("2013-06-13 10:00:00", str(dt_native))
-        self.assertIsNone(dt_native.tzinfo)
-
+        dt_native = isoToNative("2013-06-13T19:00:00+09:00")
         Relay.putRelay("product1", "serial1", "module1", 1, dt_native, True)
         Relay.putRelay("product1", "serial1", "module1", 1, dt_native, True)
         relays = Relays.getRelays("product1", "serial1", "module1")
